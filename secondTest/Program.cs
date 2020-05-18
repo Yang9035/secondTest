2ad7ddee573381f8281f11e6740547e2fc0b4503
yang9035 <yang9035@126.com>
2020-05-18 08:58:49 +0800

    添加项目文件。

---
+++ secondTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace secondTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("你好，这是我的第二次测试");
+            Console.ReadKey();
+        }
+    }
+}
